c71aa67c9499c849cdd4c49c5246a59739087d13
Bright Orton <ortonb110@gmail.com>
2023-04-02 18:34:20 +0000

    Implementing MVC Pattern

--- Controllers/product.js
+++ Controllers/product.js
@@ -1,28 +1,32 @@
-const products = [];
-const adminData = require('../routes/admin');
+const Product = require("../Models/product");
+
+const adminData = require("../routes/admin");
 exports.getAddProduct = (req, res, next) => {
-    res.render('add-product', {
-      pageTitle: 'Add Product',
-      path: '/admin/add-product',
-      formsCSS: true,
-      productCSS: true,
-      activeAddProduct: true
-    } )
-}
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+};
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
-    res.redirect('/');
-  }
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
+};
 
-  exports.getShopProducts = (req, res, next) => {
-  
-  res.render('shop', {
+exports.getShopProducts = (req, res, next) => {
+  const products = Product.fetchAll((products) => {
+    res.render("shop", {
     prods: products,
-    pageTitle: 'Shop',
-    path: '/',
+    pageTitle: "Shop",
+    path: "/",
     hasProducts: products.length > 0,
     activeShop: true,
-    productCSS: true
+    productCSS: true,
   });
-}
+  });
+  
+};
---
+++ Models/product.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
+
+  save() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (error, fileContent) => {
+      let products = [];
+      if (!error) {
+        products = JSON.parse(fileContent);
+      }
+
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (error, fileContent) => {
+      if (error) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent)) ;
+    });
+   
+  }
+};
